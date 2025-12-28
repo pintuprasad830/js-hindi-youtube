ee3a7f25c1e318a6008989f577dc27ed625bbeb9
pintuprasad830 <pintu8271264091@gmail.com>
2025-12-28 03:40:48 +0000

    varible

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,22 @@
+const accountId=144553
+let accountEmail= "hitesh@google.com"
+var accountpassword="12345"
+accountCity = "jaipur"
+  
+//accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengluru"
+console.log(accountId);
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+
+console.log([accountId, accountEmail, accountPassword, accountCity])
